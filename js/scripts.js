9d938fd06996570db7956b966b2de474eae81e89
Erica Nafziger and Taylor Loftis-Kim <taylor@>
2016-10-18 10:53:52 -0700

    paragraphs toggle between highlight and unhighlight

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".focus1").click(function() {
+    // $("p.focus1").removeClass();
+    $(".focus1").toggleClass("highlight");
+  });
+
+  $(".focus2").click(function() {
+    //$("p.focus2").removeClass();
+    $(".focus2").toggleClass("highlight");
+  });
+});
